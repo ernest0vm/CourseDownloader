37fa737a9756165e3ca66d9ec538704b4df2ad2a
Ernesto Valdez <ernesto.valdez@soportegrupocant.com>
2019-09-18 13:57:31 -0500

    add function to get the video names and rename the videos with them when downloading

--- CourseDownloader/Program.cs
+++ CourseDownloader/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace CourseDownloader
 {
@@ -10,6 +12,7 @@ namespace CourseDownloader
         {
             try
             {
+                List<string> videoNames = new List<string>();
                 string courseName;
                 int courseNumber;
                 int maxVideoIndex;
@@ -20,13 +23,13 @@ namespace CourseDownloader
 
                 Console.WriteLine("Please input the course name:");
                 courseName = Console.ReadLine();
+                Console.WriteLine();
 
                 Console.WriteLine("Please input the course index: (only numbers)");
                 courseNumber = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Please input the max index: (only numbers)");
-                maxVideoIndex = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine();
+                videoNames = GetVideos(courseNumber);
+                maxVideoIndex = Convert.ToInt32(videoNames.Count);
 
                 string pathToSave = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + $"\\{courseName}";
 
@@ -40,8 +43,9 @@ namespace CourseDownloader
 
                 for (int a = 1; a <= maxVideoIndex; a++)
                 {
+                    string videoName = $"{a}.- {videoNames[a - 1]}";
                     string source = $"http://54.147.183.92/CFTP2019/V{courseNumber}_{a}.mp4";
-                    string output = pathToSave + $"\\V{courseNumber}_{a}.mp4";
+                    string output = pathToSave + $"\\{videoName}.mp4";
 
                     if (File.Exists(output))
                     {
@@ -59,10 +63,11 @@ namespace CourseDownloader
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine($"{output} Downloaded successful.");
                     }
-                    catch
+                    catch(Exception e)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"Cannot download: {output}");
+                        Console.WriteLine($"Error Message: {e.Message}");
                     }
                 }
 
@@ -71,13 +76,64 @@ namespace CourseDownloader
                 Console.WriteLine("All videos has been downloaded successful, press any key to exit.");
                 Console.ReadLine();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"An error ocurred: {ex.Message}.");
                 Console.WriteLine("Press any key to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private static List<string> GetVideos(int courseIndex)
+        {
+            List<string> videoList = new List<string>();
+
+            try
+            {
+                Uri url = new Uri($"https://clubacademy.mx/detalle?id={courseIndex}");
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+                try
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    using (Stream stream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        string html = reader.ReadToEnd();
+
+                        var getVideos = Regex.Matches(html, "<span.*class=.lecture-title.*>(.*?)</span>");
+
+                        foreach (Match item in getVideos)
+                        {
+                            if (item.Groups.Count > 0)
+                                videoList.Add(item.Groups[1].ToString().Trim());
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine();
+                        Console.WriteLine("Video list obtained correctly!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Error while read the web: {url}. {e.Message}");
+                }
+
+                Console.WriteLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error ocurred: {ex.Message}.");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }
+
+            return videoList;
         }
     }
 }
