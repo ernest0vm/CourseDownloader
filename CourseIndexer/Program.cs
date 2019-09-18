7bae896f36f6c5c757bfccc277906e33d5199046
Ernesto Valdez <ernesto.valdez@soportegrupocant.com>
2019-09-18 12:37:18 -0500

    added number of videos for each course

--- CourseIndexer/Program.cs
+++ CourseIndexer/Program.cs
@@ -13,10 +13,10 @@ namespace CourseIndexer
             try
             {
                 StringBuilder tableCode = new StringBuilder();
-                tableCode.Append("| Index | Course Name | Time  | Category |\n");
-                tableCode.Append("| :---: | ----------- | :---: | :------: |\n");
+                tableCode.Append("| Index | Course Name | Category  | Time  | Videos |\n");
+                tableCode.Append("| :---: | ----------- | :-------: | :---: | :----: |\n");
 
-                for (int courseIndex = 1; courseIndex <= 100; courseIndex++)
+                for (int courseIndex = 1; courseIndex <= 80; courseIndex++)
                 {
                     Uri url = new Uri($"https://clubacademy.mx/detalle?id={courseIndex}");
 
@@ -33,28 +33,30 @@ namespace CourseIndexer
                             var getTitle = Regex.Match(html, "<h1.*class=.entry-title.*[^>](.*)</h1>").Groups[1];
                             var getCategory = Regex.Match(html, "<div.*class=.author-name.*(<\\s*a[^>]*>(.*?)<\\s*/\\s*a>)</div>");
                             var getTime = Regex.Match(html, "<div.*class=.total-lectures-time.*>(.*?)</div>");
+                            var getVideos = Regex.Matches(html, "<span.*class=.lecture-title.*>(.*?)</span>");
 
                             //var title = Regex.Replace(getTitle.ToString(),"\\s{32}", string.Empty);
                             var title = getTitle.ToString().Trim();
                             var category = getCategory.Groups[2];
                             var time = getTime.Groups[1];
+                            var videos = getVideos.Count;
 
-                                if (getTitle.ToString() == string.Empty)
+                            if (getTitle.ToString() == string.Empty)
                             {
                                 throw new Exception("The course don't exist!");
                             }
 
-                            tableCode.Append($"| {courseIndex} | <a href=\"{url}\" target=\"_blank\">{title}</a> | {time} | {category} |\n");
+                            tableCode.Append($"| {courseIndex} | <a href=\"{url}\" target=\"_blank\">{title}</a> | {category} | {time} | {videos} |\n");
 
                             Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Url: {url}, Title: {title}, Index: {courseIndex}, Category: {category}, Time: {time}");  
+                            Console.WriteLine($"Url: {url}, Title: {title}, Index: {courseIndex}, Category: {category}, Time: {time}, Videos: {videos}");
                         }
                     }
                     catch (Exception e)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine($"Error while read the web: {url}. {e.Message}");
-                        tableCode.Append($"| {courseIndex} | {e.Message} | no time! | no category! |\n");
+                        tableCode.Append($"| {courseIndex} | {e.Message} | no category! | no time! | no videos! |\n");
                     }
                 }
 
